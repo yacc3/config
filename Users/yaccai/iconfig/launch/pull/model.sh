0d7b138b2f169525392b8a59ca281f80fdc7cca8
yaccai <yaccai@MBP.local>
2019-02-24 05:00:17 +0800

    2019-02-24 05:00:16

---
+++ Users/yaccai/iconfig/launch/pull/model.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# coding:utf-8
+
+/bin/date +'model @ %Y-%m-%d %T ...'
+
+test -d /Volumes/Store/Model || exit 0
+for url in  https://www.nvshens.com/girl/22162/album/ \
+            https://www.nvshens.com/girl/17936/album/ \
+            https://www.nvshens.com/girl/24936/album/ \
+            https://www.nvshens.com/girl/23448/album/ \
+            https://www.nvshens.com/girl/20763/album/ \
+            https://www.nvshens.com/girl/25666/album/ \
+            https://www.nvshens.com/girl/24691/album/ \
+            ; do
+    ~/iconfig/shell/media.sh nvshens "$url" &>/dev/null
+done
+
+echo "done"
+echo
