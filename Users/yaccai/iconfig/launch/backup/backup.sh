e970c03c2847124b9dd326deca87be7ebd58c940
yacc3 <yacc3@github.desktop>
2019-03-20 05:00:29 +0800

    2019-03-20 05:00:27

--- Users/yaccai/iconfig/launch/backup/backup.sh
+++ Users/yaccai/iconfig/launch/backup/backup.sh
@@ -18,6 +18,7 @@ cat ~/iconfig/launch/backup/config.include | while read it; do
 done 
 
 fping -t 200 114.114.114.114 &>/dev/null && {
+    echo "git  push"
     for repo in /Volumes/Store/Code/yacc3.github.io  \
                 /Volumes/Store/Code/yacc3.github.src \
                 /Volumes/Store/Config; do
