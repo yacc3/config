1203bfe7cf3571302334386dddb3fb86c6d88172
yacc3 <yacc3@github.desktop>
2019-04-18 05:00:24 +0800

    2019-04-18 05:00:22

--- Users/yaccai/iconfig/launch/check/check03m.sh
+++ Users/yaccai/iconfig/launch/check/check03m.sh
@@ -4,12 +4,12 @@
 /bin/date +'check @ %Y-%m-%d %T ...'
 
 test -d /Volumes/Store && {
-    /usr/local/bin/gfind /Volumes/Store/Downloads/t66ydone -type f -size -80M -delete
+    /usr/local/bin/gfind /Volumes/Store/Downloads/t66ydone -type f -size -150M -delete
     /usr/local/bin/gfind /Volumes/Store/Downloads/t66ydone -type f -print0 \
         | /usr/local/bin/gxargs -0 -I {} mv {} /Volumes/Store/Torrent/10V
     /usr/local/bin/gfind /Volumes/Store/Downloads/t66ydone -depth -mindepth 1 -type d -delete
 
-    /usr/local/bin/gfind /Volumes/Store -maxdepth 1 -type f -size +80M -print0 \
+    /usr/local/bin/gfind /Volumes/Store -maxdepth 1 -type f -size +150M -print0 \
         | /usr/local/bin/gxargs -0 -I {} mv {} /Volumes/Store/Torrent/10V
     
     # /usr/local/bin/gfind /Volumes/Store/Torrent/10V/t66y_html -regextype "egrep" -regex ".*/[0-9]{7}\.html" -ctime +3 -delete
