5b82a9172a495254a55acaf75eefceaf4b2a310d
yacc3 <yacc3@github.desktop>
2019-08-07 05:00:22 +0800

    2019-08-07 05:00:21

--- Users/yaccai/iconfig/launch/check/check60m.sh
+++ Users/yaccai/iconfig/launch/check/check60m.sh
@@ -5,7 +5,7 @@
 
 chflags hidden ~/Onedrive
 
-/usr/local/bin/python3  ~/iconfig/exe/caoliu.py
+# /usr/local/bin/python3  ~/iconfig/exe/caoliu.py
 # /usr/local/bin/gfind ~/Downloads -regextype 'egrep' -regex '.*/[0-9a-z]{20,60}*.torrent' | while read it; do
 #     open -ga Thunder "$it"
 #     sleep 0.5
